f2c8fc0b62784ed4ef8a3f84b5ef985b96aa0e61
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-01-22 20:31:11 +1100

    Print statements

---
+++ Print statements.py
@@ -0,0 +1,15 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a integer
+print(4)
+
+#line breaking
+print("Hello python\n")
+
+#Pass multiple arguments for printing
+print("hello", 5)
+
+#End argument of print statement
+print("welcome to ", end= "*")
+
