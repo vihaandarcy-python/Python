4ae6ab230d1fba1cc22644f89f3447ffbf68c86e
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-01-23 16:41:27 +1100

    variables homework

---
+++ print statements homework.py
@@ -0,0 +1,18 @@
+#storing values in variables
+
+Vihaan = ("\nVihaan - 15/03/2012\n")
+
+Dad = ("\nDad - 31/08/1977\n")
+
+Mom = ("\nMom- 09/11/1982\n")
+
+Granddad = ("\nGrandad - 07/01/1954\n")
+
+Grandma = ("\nGrandma - 01/10/1958")
+
+
+
+
+
+
+print("The five birthdates are...\n", Vihaan, Dad, Mom, Granddad, Grandma)
