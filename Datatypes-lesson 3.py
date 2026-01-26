db33620fe84520fbb550c9f1ca0c3310840e24d8
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-01-26 13:53:22 +1100

    project datatypes

---
+++ Datatypes-lesson 3.py
@@ -0,0 +1,41 @@
+#Assigning different Variables
+
+name = "penguin"
+
+age = 15
+
+is_student = True
+
+weight = 38.5
+
+
+#prininting different variables in their datatypes
+
+print("Name: ", name)
+print("Data type of name is- ", type(age))
+print("\n")
+
+print("age: ", age)
+print("Data type of age is- ", type(age))
+print("\n")
+
+print("is_student: ", is_student)
+print("data type of is_student is- ", type(is_student))
+print("\n")
+
+print("weight: ", weight)
+print("Data type of weight is- ", type(weight))
+print("\n")
+
+#type casting, converting the datatype of varibales
+
+print("\n After type casting...")
+age = str(age)
+print(age)
+print("Data type of age is: ", type(age))
+
+print("\n After type casting...")
+weight = int(weight)
+print(weight)
+print("datatype of weight is: ", type(weight))
+
