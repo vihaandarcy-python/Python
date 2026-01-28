6f06e8612d3b5f6b4732124314c09eef8ddbe089
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-01-28 16:45:52 +1100

    datatyoes hw

---
+++ Datatypes hw.py
@@ -0,0 +1,18 @@
+#Convert a string into upper case
+x = "coNgrAtuLaTioNS"
+
+print(str.lower(x))
+print(str.upper(x))
+
+# Take two strings as input from the user
+print("Enter the first string: ")
+strOne = input()
+
+print("Enter the second string: ")
+strTwo = input()
+
+#Concatinate the other two strings
+#using Addition operator
+strThree = strOne + " " + strTwo
+
+print("\n Concatienated String: ", strThree)
