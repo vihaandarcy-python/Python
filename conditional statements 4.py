f5e9b4b7cd1f69b97e1743dc30f8b66636cc5edc
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-01-29 15:35:16 +1100

    conditional task 4

---
+++ conditional statements 4.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a Number to check"))
+print("Number to be checked{0}\n".format(number))
+
+if number%2==0:
+    print("This is an even Number")
+else: 
+    print("This is an odd number")
