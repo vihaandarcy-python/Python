b29dc299a4be80d1c82c41320e450c22d8d6fca1
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-01-29 15:29:32 +1100

    Conditional task 3

---
+++ conditional statements 3.py
@@ -0,0 +1,11 @@
+#indentations
+
+i= int(input("enter a number : "))
+if(i<15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+else:
+    print("i is greater than 15")
+    print("i'm in else Block")
+
+print("I'm not in if block and neither in the else block")
