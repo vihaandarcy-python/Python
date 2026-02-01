1d8c881df3d8e3469b2cf63cec93df6a6bc08b74
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-02 08:15:48 +1100

    operators hw

---
+++ operators hw.py
@@ -0,0 +1,8 @@
+num = 49
+num_sqrt = num**0.5
+print(f'the number is {num} and it is stored in num_sqrt {num_sqrt}')
+
+#Python Program to calculate the square root
+num = int(input("Please enter your number:"))
+num_sqrt = num**0.5
+print('The square root of %0.1f is %0.2f' % (num, num_sqrt))
