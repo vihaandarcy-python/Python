581f170e7d96d5f39b00ea58971de211334d52b1
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-02 16:55:02 +1100

    operators 2, task2

---
+++ operators 2, task 2.py
@@ -0,0 +1,25 @@
+a = 10
+b = 12
+c = b
+
+print(a != b)
+print (b != c)
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print( a, 'and', b, 'are different.\n')
+
+a = 4
+b = 5
+
+if (a==1) != (b==5):
+    print('hello\n\n')
+
+a = int(input("Please enter a number: "))
+if a%2 != 0 :
+    print(a, "is not an even number")
+
+else:
+    print("Your number is an even number!")
