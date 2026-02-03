1f18cd18d9a3e55569410d52e873daa0777ff9cf
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-03 18:15:53 +1100

    operators 3 homework

---
+++ operators 3 hw.py
@@ -0,0 +1,58 @@
+x = int(input("enter the ASCII code between 65-90: "))
+
+if x==65:
+    print("The ASCII Code 65 stands for Alphabet A")
+
+elif x==66:
+    print("The ASCII Code 65 stands for Alphabet B ")
+elif x==67:
+    print("The ASCII Code 65 stands for Alphabet C")
+elif x==68:
+    print("The ASCII Code 65 stands for Alphabet D")
+elif x==69:
+    print("The ASCII Code 65 stands for Alphabet E")
+elif x==70:
+    print("The ASCII Code 65 stands for Alphabet F")
+elif x==71:
+    print("The ASCII Code 65 stands for Alphabet G")
+elif x==72:
+    print("The ASCII Code 65 stands for Alphabet H")
+elif x==73:
+    print("The ASCII Code 65 stands for Alphabet I")
+elif x==74:
+    print("The ASCII Code 65 stands for Alphabet J")
+elif x==75:
+    print("The ASCII Code 65 stands for Alphabet K")
+elif x==76:
+    print("The ASCII Code 65 stands for Alphabet L")
+elif x==77:
+    print("The ASCII Code 65 stands for Alphabet M")
+elif x==78:
+    print("The ASCII Code 65 stands for Alphabet N")
+elif x==79:
+    print("The ASCII Code 65 stands for Alphabet O")
+elif x==80:
+    print("The ASCII Code 65 stands for Alphabet P")
+elif x==81:
+    print("The ASCII Code 65 stands for Alphabet Q")
+elif x==82:
+    print("The ASCII Code 65 stands for Alphabet R")
+elif x==83:
+    print("The ASCII Code 65 stands for Alphabet S")
+elif x==84:
+    print("The ASCII Code 65 stands for Alphabet T")
+elif x==85:
+    print("The ASCII Code 65 stands for Alphabet U")
+elif x==86:
+    print("The ASCII Code 65 stands for Alphabet V")
+elif x==87:
+    print("The ASCII Code 65 stands for Alphabet W")
+elif x==88:
+    print("The ASCII Code 65 stands for Alphabet X")
+elif x==89:
+    print("The ASCII Code 65 stands for Alphabet Y")
+elif x==90:
+    print("The ASCII Code 65 stands for Alphabet Z")
+
+else:
+    ("Please enter a valid input")
