53f81d92696584abe5cb25b68f79d6e4fc32f3f0
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-09 17:01:09 +1100

    last lesson task 3

---
+++ last lesson task 3.py
@@ -0,0 +1,17 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+
+#sum of 40 numbers
+sum = mean1*total_numbers
+print("the sum of 40 numbers: ", sum)
+
+
+#correct sum of these numbers
+num2= sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)): ", num2)
+
+#the correct mean
+mean2 = num2/total_numbers
+print("The correct mean is ",mean2)
