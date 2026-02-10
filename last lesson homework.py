87fa2c6942f82f919bf80c2c7cd388721033fe45
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-10 18:41:08 +1100

    last lesson hw

---
+++ last lesson homework.py
@@ -0,0 +1,12 @@
+#Take input from user
+x = input("Enter value of x:")
+y = input("Enter Value of y:")
+
+#swapping
+temp = x
+x = y
+y = temp
+
+#Displaying results after swapping
+print("value of x after swapping is", x)
+print("value of y after swapping is", y)
