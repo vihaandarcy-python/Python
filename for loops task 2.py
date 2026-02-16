889f7a229fbf8b90de9a622a3f8aebcc0a12578c
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-16 17:00:14 +1100

    for loops task 2

---
+++ for loops task 2.py
@@ -0,0 +1,9 @@
+string = input("Please eneter your own string: ")
+
+string2 = (" ")
+
+for i in string:
+    string2 = i + string2
+
+print("\nthe original string was: ", string)
+print("the reversed string is: ", string2)
