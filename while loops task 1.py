cac0060c56a8d22653a87959c5db9e7b96cc9992
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-02-17 18:57:18 +1100

    while loops task 1

---
+++ while loops task 1.py
@@ -0,0 +1,10 @@
+#input the value of terms from the user
+n=int(input("Enter the value of terms: "))
+
+sum=0
+i=1
+while i<=n: #loops will run from 1 to n
+    sum = sum+i
+    i= i+1
+
+print("\nSum =", sum)
