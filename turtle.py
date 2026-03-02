23e76d1eebdf5db20d8c9a13a518b4c9c90181cf
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-02 19:55:29 +1100

    turtle

---
+++ turtle.py
@@ -0,0 +1,15 @@
+import turtle
+#inport the turtle library
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+polygon = tutle.Turtle() #defined variable
+
+num_sides = 6 #variable
+side_length = 70 
+angle = 360.0/ num_sides
+#iterate loop for total number of side
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
+
+turtle.done()
