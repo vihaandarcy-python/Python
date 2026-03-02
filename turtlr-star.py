cc1d5be98e88f2cf9f4dc581f261c292f13e4b39
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-02 20:08:25 +1100

    turter star

---
+++ turtlr-star.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+
+#first triangle for star
+board.forward(100) #draw base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+#second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
