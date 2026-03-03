542cd5bbf3ec76ec52f11e2f1135234a4b93135e
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-03 19:08:12 +1100

    functions task 2

---
+++ functions task 2.py
@@ -0,0 +1,8 @@
+def weather_condition():
+    print("The weather is pleasant in", a, "at a temperature of 23 degrees celcius")
+    print("The weather is same in", b)
+
+a= "autum"
+b= "spring"
+
+weather_condition()
