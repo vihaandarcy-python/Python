14d8a827d5811eeeb95f5e7c793eb6ebe5fe5221
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-10 19:14:02 +1100

    arguments task 2

---
+++ arguments task 2.py
@@ -0,0 +1,15 @@
+#define function to calculte cube
+def cube(number):
+    return number*number*number
+
+#define a function which will execute cibe function if user 
+#enteres number is divisible by 3
+
+def by_three(number):
+    if number % 3 == 0:
+        return cube(number)
+    else:
+        return False
+#display result
+print(by_three(9))
+print(by_three(4))
