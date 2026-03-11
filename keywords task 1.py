1782580a093d5878205cdc66b1a1d5399a1ded1b
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-11 19:43:42 +1100

    keywords task 1

---
+++ keywords task 1.py
@@ -0,0 +1,11 @@
+#take input from the user
+a = input ("Enter a word: ")
+#program to check break keyword
+for i in a: #iterate for loop
+    if (i=='A'):#condition 1
+        #display result
+        print ("A if found")
+        break #break statement
+    else:
+        #display result
+        print ("A not found")
