713c2b2b6aba9a6065a5be61a20f7793a3c5da1d
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-11 19:51:27 +1100

    keyword task 2

---
+++ keywords task 2.py
@@ -0,0 +1,16 @@
+for x in range(10): #iterate loop
+    if x % 20 == 0:   #condition 1
+        print("twist")
+
+    elif x % 15 == 0: #condition 2
+        pass #pass statement
+
+    elif x % 5 == 0: #condition 3
+        print("fizz") #display result
+
+    elif x % 3 == 0: #condition 4
+        print("buzz") #display result
+
+    else:
+        print(x) #condition 5 display result
+             
