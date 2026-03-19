36de3130c864bc38dad40db24c4acade5ffc56b6
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-19 18:23:33 +1100

    random hw

---
+++ random hw.py
@@ -0,0 +1,11 @@
+#importing math
+#using math function for calculating trignometric values
+import math
+
+a = math.sin(0.5)
+a = math.cos(0.5)
+a = math.tan(0.5)
+
+print("Value of sin(0.5) = ",a)
+print("Value of cos(0.5) = ",a)
+print("Value of tan(0.5) = ",a)
