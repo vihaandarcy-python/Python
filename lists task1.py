7af299525e8a8608f19e8ac2e83f0a4d5eb256dd
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-03-25 19:46:28 +1100

    List task 1

---
+++ lists task1.py
@@ -0,0 +1,15 @@
+empty_list = []
+print()
+
+#A list of numbers
+number = [1, 2, 3, 4, 5]
+print(number)
+
+#use  * operator
+triples = [1, 2, 3] * 3
+print(triples)
+
+#reverse the given list
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"\n")
