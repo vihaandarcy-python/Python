75a17c509c39425e521949b7e5c93854e47031a8
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-04-01 19:49:09 +1100

    dictioanry task 3

---
+++ dictionary task3.py
@@ -0,0 +1,9 @@
+country_code = {'India' : '0091',
+                'Australia' : '0061',
+                 'Nepal' : '0097'}
+
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
