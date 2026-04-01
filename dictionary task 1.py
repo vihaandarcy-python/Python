dd23c84d1a47a6a24486a0a55b2636821872fa61
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-04-01 19:41:19 +1100

    dictionary task 1

---
+++ dictionary task 1.py
@@ -0,0 +1,12 @@
+test_dict = {'codingal' :2, 'is':2, 'best':2, 'for':2, 'coding':1}
+
+print("The origional dictionary : " + str(test_dict))
+
+k = 2
+
+res = 0
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res + 1
+
+print("Frequency of K is: " + str(res))
