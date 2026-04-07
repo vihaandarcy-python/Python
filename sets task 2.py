9c761e1be713df87615078987a6e8170ba997a3a
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-04-07 18:07:05 +1000

    sets task 2

---
+++ sets task 2.py
@@ -0,0 +1,11 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Origional set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz2 = setx.intersection(sety)
+print(setz2)
+
+setz1 = setx.union(sety)
+print(setz1)
