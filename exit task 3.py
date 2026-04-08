e564ea76b45cea523ec0cb698e902afc5b8b928f
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-04-08 18:59:18 +1000

    exit task 3

---
+++ exit task 3.py
@@ -0,0 +1,11 @@
+#python program to demonstrate
+#exit()
+for i in range(10):
+    #if the value of i becomes
+    #5 then the program is forced 
+    #to exit
+    if i == 5:
+        #prints the exit message
+        print(exit)
+        exit
+        
