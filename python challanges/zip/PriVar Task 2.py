734815f6d5b5b21d71c774ff4d51f61df947c682
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-04-29 18:53:57 +1000

    Task 2

---
+++ python challanges/zip/PriVar Task 2.py
@@ -0,0 +1,18 @@
+class computer:
+    
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print("Selling Price: {}".format(self.__maxprice))
+
+    def setMaxprice(self, price):
+        self.__maxprice = price
+
+
+c = computer()
+c.sell()
+c.__maxprice = 100
+c.sell()
+c.setMaxprice(1000)
+c.sell()
