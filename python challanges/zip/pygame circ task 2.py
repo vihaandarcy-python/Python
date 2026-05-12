db2b587ec713fc3c631017eff0ac6ad286f91103
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-05-12 18:02:33 +1000

    pygame task 2

---
+++ python challanges/zip/pygame circ task 2.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((400, 400))
+
+window.fill((255, 255, 255))
+
+GREEN = (0, 255, 0)
+
+pygame.draw.circle(window, GREEN, (300, 300), 50)
+pygame.draw.circle(window, GREEN, (100, 100), 50, 3)
+
+pygame.display.update()
+
+Running = True
+
+while Running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Running = False
+
+pygame.QUIT()
