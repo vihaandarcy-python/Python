363652987a917fcb5654ae226fd97afb2d28a107
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-07-13 06:34:55 +1000

    binary

---
+++ python challanges/zip/binary finder.py
@@ -0,0 +1,19 @@
+def BinaryToDecimal(binary):
+
+
+    decimal, i = 0, 0
+
+
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary//10
+
+        i += 1
+    print("Decimal : ",decimal)
+
+
+binary = int(input("Enter your Binary: "))
+
+BinaryToDecimal(binary)
+
