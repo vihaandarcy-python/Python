d8ee364f7f2e661bd102cdb60848a3936c6c4caa
vihaandarcy-python <vihaan.darcy@gmail.com>
2026-09-14 17:51:17 +1000

    array hw

---
+++ python challanges/zip/array reverse.py
@@ -0,0 +1,9 @@
+def array_flip(a, asize):
+
+        print(a[::-1],", ")
+
+a = (367, 73828, 373, 31, 34, 4, 2, 5, 1, 78)
+
+
+# to put the array in a descending order just do
+#sorted(a) to then print backwards
